3c2e60b364d0dca736256c8b1347e22490b4455b
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-05-12 14:42:20 +0530

    Importing Random

--- Guess_the_Number.py
+++ Guess_the_Number.py
@@ -1,1 +1,12 @@
+# Tried to make a Guessing a Number Game
+
 import random
+
+print("Hello There , Enter your Name")
+name = input()
+print("Welcome", name)
+print("Hey,",name,"I am Thinking of a Number Between 1 and 30")
+secretNumber = random.randint(1,30)
+
+
+
