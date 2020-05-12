fb1ec36dd486f3cd168c6883ce15e2558ce7aafd
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-05-12 14:51:34 +0530

    Body of the Code

--- Guess_the_Number.py
+++ Guess_the_Number.py
@@ -5,8 +5,22 @@ import random
 print("Hello There , Enter your Name")
 name = input()
 print("Welcome", name)
-print("Hey,",name,"I am Thinking of a Number Between 1 and 30")
-secretNumber = random.randint(1,30)
+print("Hey,", name, "I am Thinking of a Number Between 1 and 30")
+secretNumber = random.randint(1, 30)
 
+for guesses in range(6):
+    print("Take a Guess.")
+    guess = int(input())
 
+    if guess < secretNumber:
+        print("Your guess is Lower than the Number")
+    elif guess > secretNumber:
+        print("Your Guess in Higher than the Number")
+    else:
+        break
 
+if guess == secretNumber:
+    print("Woah", name, "You Guessed it Right", "You Guessed in", guesses, "times")
+
+else:
+    print("No the Right Number was ", str(secretNumber))
