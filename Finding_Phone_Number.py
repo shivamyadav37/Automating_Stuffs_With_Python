c736ec7aa713170b9cdbe6310b8f8593846ba7e5
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-05-13 15:52:20 +0530

    New Commit

--- Finding_Phone_Number.py
+++ Finding_Phone_Number.py
@@ -3,12 +3,14 @@
 # Importing regex module
 import re
 
-message = "Hello this is my Phone number , 404-231-9384"
+message = "Hello this is my Phone number , 404-231-9384 , So call me at 404-231-9384"
 
 # Regex Expression
 phoneNumber = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-# Compile Fuction passes the String
+# Compile Function passes the String
 
-x = phoneNumber.search(message)
+# x = phoneNumber.search(message)
+x = phoneNumber.findall(message)
 
-print("Phone Number is:", x.group())
+# print("Phone Number is:", x.group())
+print(x)
