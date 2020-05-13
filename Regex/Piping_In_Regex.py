2426a978d3dc154ebf1d4007810f08aa6a906c7c
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-05-13 16:31:03 +0530

    New Commit

--- Regex/Piping_In_Regex.py
+++ Regex/Piping_In_Regex.py
@@ -1,5 +1,10 @@
 # Created by Shivam Yadav at 13-05-2020
+import re
 
+messageString = "Hello Alfred , Please Park the Batmobile , and get the Batcopter fixed"
 
+batRegex = re.compile(r'Bat(man|mobile|copter)')
 
+mo = batRegex.search(messageString)
 
+print("Found", mo.group())
