cbcdd0128ba827bfec3a293d28e8c1294f7ba477
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-05-13 16:00:15 +0530

    New Commit

--- Finding_Phone_Number.py
+++ Finding_Phone_Number.py
@@ -9,8 +9,10 @@ message = "Hello this is my Phone number , 404-231-9384 , So call me at 404-231-
 phoneNumber = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 # Compile Function passes the String
 
-# x = phoneNumber.search(message) # Search finds only one time
-x = phoneNumber.findall(message) # findall finds all the Matching Strings
+x = phoneNumber.search(message)  # Search finds only one time
+# x = phoneNumber.findall(message)  # findall finds all the Matching Strings
 
-# print("Phone Number is:", x.group()) # Use this in Case of using 'search'
-print(x)
+print("Phone Number is:", x.group())  # Use this in Case of using 'search'
+# print(x)
+
+
