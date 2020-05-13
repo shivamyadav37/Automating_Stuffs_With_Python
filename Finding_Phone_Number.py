c7e9bbcdc557ec11a010b472a28f3216380fbe26
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-05-13 15:49:22 +0530

    New Commit

--- Finding_Phone_Number.py
+++ Finding_Phone_Number.py
@@ -7,6 +7,7 @@ message = "Hello this is my Phone number , 404-231-9384"
 
 # Regex Expression
 phoneNumber = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# Compile Fuction passes the String
 
 x = phoneNumber.search(message)
 
