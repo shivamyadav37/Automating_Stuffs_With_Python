878a2cfd251a5a8dfe894e8826182d0569216a88
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-05-13 16:11:20 +0530

    New Commit

--- Finding_Phone_Number.py
+++ Finding_Phone_Number.py
@@ -13,6 +13,8 @@ x = phoneNumber.search(message)  # Search finds only one time
 # x = phoneNumber.findall(message)  # findall finds all the Matching Strings
 
 print("Phone Number is:", x.group())  # Use this in Case of using 'search'
-# print(x)
+# print(x) # Use this in case of findall
 
-
+phoneNumber1 = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+y = phoneNumber1.search(message)
+print("Area Code for the Phone Number is :", y.group(1))
