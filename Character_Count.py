c2a7d437040a510a5e11d6473d76e7fbb3d478aa
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-05-13 13:47:11 +0530

    New Commit

--- Character_Count.py
+++ Character_Count.py
@@ -1,6 +1,6 @@
 # This Program Counts Number of Characters Passed in 'message'(String) Variable
 
-message = 'Hello alice , how ya doing , have a great day ahead' #you can have your own custom message
+message = input() #you can have your own custom message
 count = {}
 
 for character in message:
