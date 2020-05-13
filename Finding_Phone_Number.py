237c03e2e4077ef8d19f53b9bf8862ef929fca23
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-05-13 15:48:05 +0530

    New Commit

--- Finding_Phone_Number.py
+++ Finding_Phone_Number.py
@@ -1,7 +1,11 @@
+# This Program demonstrates to find USA Phone numbers.
+
+# Importing regex module
 import re
 
 message = "Hello this is my Phone number , 404-231-9384"
 
+#Regex Expression
 phoneNumber = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 
 x = phoneNumber.search(message)
