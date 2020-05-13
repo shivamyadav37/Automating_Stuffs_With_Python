f6b847a3fd033f82bc7ab92d4b458fcfd08a1660
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2020-05-13 15:53:31 +0530

    New Commit

--- Finding_Phone_Number.py
+++ Finding_Phone_Number.py
@@ -10,7 +10,7 @@ phoneNumber = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 # Compile Function passes the String
 
 # x = phoneNumber.search(message) # Search finds only one time
-x = phoneNumber.findall(message)
+x = phoneNumber.findall(message) # findall finds all the Matching Strings
 
 # print("Phone Number is:", x.group()) # Use this in Case of using 'search'
 print(x)
